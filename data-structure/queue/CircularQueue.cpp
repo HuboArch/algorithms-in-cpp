67dd1e9220ad5d850ce66ef92901e9bca1db3441
agribeau <hkissbigapple@163.com>
2018-08-08 10:18:38 +0800

    feat(circular queue): Create class CircularQueue

---
+++ data-structure/queue/CircularQueue.cpp
@@ -0,0 +1,5 @@
+//
+// Created by dean on 2018/8/8.
+//
+
+#include "header/CircularQueue.h"
---
+++ data-structure/queue/header/CircularQueue.h
@@ -0,0 +1,37 @@
+//
+// Created by dean on 2018/8/8.
+//
+
+#ifndef ALGORITHMS_IN_CPP_CIRCULARQUEUE_H
+#define ALGORITHMS_IN_CPP_CIRCULARQUEUE_H
+
+
+class CircularQueue
+{
+public:
+    CircularQueue(int m_iCapacity); // 创建队列
+
+    virtual ~CircularQueue();       // 销毁队列
+
+    void ClearQueue();              // 清空队列
+
+    bool IsEmpty() const;           // 判断是否为空
+
+    int GetLength() const;          // 获取队列长度
+
+    bool EnQueue(int elem);         // 队尾添加元素
+
+    bool DeQueue(int& elem);        // 队头删除元素
+
+    void Traverse();                // 遍历队列
+
+private:
+    int *m_pQueue;      // 队列数组指针
+    int m_ilength;      // 队列长度
+    int m_iCapacity;    // 队列容量
+    int m_iHead;        // 队头
+    int m_iTail;        // 队尾
+};
+
+
+#endif //ALGORITHMS_IN_CPP_CIRCULARQUEUE_H
